93ff66eb07cc6ca31a0eab5a603ef5ecbbfc02e9
Saad Koubeissi <saad@koubeissi.com>
2022-12-29 00:37:23 +0400

    feat: added req controller to avoid use effect remounting

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,24 @@ import { useEffect } from 'react'
 
 function App() {
   useEffect(() => {
+    // the reqController is to avoid the useEffect remounting strict mode feature
+    const reqController = new AbortController()
+
     async function go() {
-      const picsPromise = await fetch('http://localhost:8000/news')
-      const pics = await picsPromise.json()
-      console.log({pics})
+      try {
+        const picsPromise = await fetch('http://localhost:8000/news', {signal: reqController.signal})
+        const pics = await picsPromise.json()
+        console.log({pics})
+      } catch {
+        console.log("our request was canceled")
+      }
     }
     go()
 
     // cleanup after unmounting...
-    // return () => {
-      
-    // }
+    return () => {
+      reqController.abort()
+    }
   }, [])
 
   return (
